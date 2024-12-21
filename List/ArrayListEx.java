8cc8b231f0f371ee9ad6443bad01eef7cd0cc44f
jagdish gupta <varshneyjacky@gmail.com>
2024-12-21 21:58:55 +0530

    collection framework from vipul tyagi -enginerr digest

---
+++ List/ArrayListEx.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEx{
+    public static void main(String[] args) {
+            List<Integer> list= new ArrayList<>();
+            list.add(5);
+            list.add(10);
+            System.out.println(list.size());    
+            
+            
+    }
+}
